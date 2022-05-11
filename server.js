f813717b8fbc39c49c03069df320ed4f8f371f2d
LouiseMillan <milouisedu@gmail.com>
2022-05-11 16:48:57 -0500

    API CRUD de ejemplo completado

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const req = require("express/lib/request");
 
 const prisma = new PrismaClient();
 const app = express();
@@ -9,6 +10,59 @@ app.use(express.json());
 
 app.get("/", (request, response) => {
     response.json({message: "alive"});
+});
+
+app.get("/explorers", async (request, response) => {
+    const allExplorers = await prisma.explorer.findMany({});
+    response.json(allExplorers);
+});
+
+app.get("/explorers/:id", async (request, response) => {
+    const id = request.params.id;
+    const explorer = await prisma.explorer.findUnique({
+        where: {
+            id: parseInt(id)
+        }
+    });
+    response.json(explorer);
+});
+
+app.post("/explorers", async (request, response) => {
+    const explorer = {
+        name: request.body.name, 
+        username: request.body.username,
+        mission: request.body.mission
+    };
+
+    await prisma.explorer.create({data: explorer});
+    return response.json({message: "Explorer creado."});
+});
+
+app.put("/explorers/:id", async (request, response) => {
+    const id = parseInt(request.params.id);
+
+    await prisma.explorer.update({
+        where:{
+            id: id
+        },
+        data: {
+            mission: request.body.mission
+        }
+    });
+
+    return response.json({message: "Actualizado correctamente."});
+});
+
+app.delete("/explorers/:id", async (request, response) => {
+    const id = parseInt(request.params.id);
+
+    await prisma.explorer.delete({
+        where: {
+            id: id
+        }
+    });
+
+    return response.json({message: "Eliminado correctamente."});
 });
 
 app.listen(port, () => {
